777d8516c38bfa15c269acec48294e57b706d418
Nishma Maskey <maskeynishma@gmail.com>
2019-03-19 21:02:42 -0500

    initial

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header() {
+    return (<div>
+        <header className="navbar">This is the header</header>
+        <p> Women in Technology </p>
+        </div>
+    )
+}
+
+export default Header
